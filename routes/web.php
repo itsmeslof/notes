45ccc058acd161cb20a5d2c7807fe7f9c708ee6a
Slof <slof@hey.com>
2022-12-05 23:34:18 -0600

    Cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\CustomMarkdownConverter;
+use App\HTMLPurifier;
 use App\Http\Controllers\NotebookController;
 use App\Http\Controllers\NotebookPageController;
 use App\Http\Controllers\Notebooks\BookmarkController;
 use App\Models\Notebook;
+use App\Models\NotebookPage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,20 +23,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('notebooks.index');
+});
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('notebooks', NotebookController::class)->withTrashed();
 
     Route::prefix('/notebooks/{notebook}')->as('notebooks.')->group(function () {
         Route::patch('/restore', [NotebookController::class, 'restore'])->name('restore')->withTrashed();
-
-        Route::post('/bookmark', [BookmarkController::class, 'store'])->name('bookmark.store')->withTrashed();
-        Route::delete('/bookmark/destroy', [BookmarkController::class, 'destroy'])->name('bookmark.destroy')->withTrashed();
+        Route::patch('/bookmark', [BookmarkController::class, 'update'])->name('bookmark.update')->withTrashed();
     });
 
-    // Route::resource('notebooks.pages', NotebookPageController::class)->scoped([
-    //     'page' => 'hashid'
-    // ]);
-});
+    Route::resource('notebooks.pages', NotebookPageController::class)->withTrashed();
 
+    Route::prefix('/notebooks/{notebook}/pages/{page}')->as('notebooks.pages.')->group(function () {
+        Route::patch('/restore', [NotebookPageController::class, 'restore'])->name('restore')->withTrashed();
+    });
+});
 
 require __DIR__ . '/auth.php';
