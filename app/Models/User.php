81f6e93edbdee0612b155ffe5821519bf7e4d994
Slof <slof@hey.com>
2023-05-26 01:50:06 -0500

    Update user model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+        'metadata'
     ];
 
     /**
@@ -40,5 +42,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'metadata' => 'array'
     ];
 }
