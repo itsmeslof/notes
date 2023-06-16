191f604edcb11a51b764d96ad4cf3dafa6d088e2
Slof <slof@hey.com>
2023-06-16 06:43:34 -0500

    Create NotePolicy.php

---
+++ app/Policies/NotePolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class NotePolicy
+{
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Note $note): Response
+    {
+        return $user->is($note->user) ? Response::allow() : Response::denyWithStatus(404);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Note $note): Response
+    {
+        return $user->is($note->user) ? Response::allow() : Response::denyWithStatus(404);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Note $note): Response
+    {
+        return $user->is($note->user) ? Response::allow() : Response::denyWithStatus(404);
+    }
+}
