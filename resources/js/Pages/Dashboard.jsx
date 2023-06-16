82e00478bfe18f1e3326a0c533127fa5fc6579f6
Slof <slof@hey.com>
2023-06-16 12:22:43 -0500

    Update Dashboard.jsx

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,19 +1,83 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import StandardLink from "@/Components/StandardLink";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
-export default function Dashboard({ auth }) {
+export default function Dashboard({ auth, recentNotes }) {
+    const recentNotesList = () => {
+        const noNotes = (
+            <li>
+                No recent notes <a href="/notes/create">Create One Now</a>
+            </li>
+        );
+
+        return !recentNotes.length
+            ? noNotes
+            : recentNotes.map((note) => {
+                  return (
+                      <li key={note.hashid} className="text-neutral-300">
+                          <StandardLink
+                              href={`/notes/${note.hashid}`}
+                              className="underline hover:text-neutral-100"
+                          >
+                              {note.metadata?.title || "Untitled Note"}
+                          </StandardLink>
+                      </li>
+                  );
+              });
+    };
+
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
-        >
+        <AuthenticatedLayout>
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <h2 className="text-neutral-100 text-2xl font-bold">
+                        Links on this page:
+                    </h2>
+                    <ul className="list-disc list-inside mt-4 ml-4 space-y-1 text-neutral-300">
+                        <li>
+                            <StandardLink
+                                href="/notes/create"
+                                className="underline text-teal-500 hover:text-teal-400"
+                            >
+                                Create a new Note
+                            </StandardLink>
+                        </li>
+                        <li>
+                            <StandardLink
+                                href="/notes"
+                                className="underline hover:text-neutral-100"
+                            >
+                                View all Notes
+                            </StandardLink>
+                        </li>
+                        <li>
+                            <StandardLink
+                                href="/markdown-reference"
+                                className="underline hover:text-neutral-100"
+                            >
+                                Markdown Reference
+                            </StandardLink>
+                        </li>
+                        <li>
+                            <StandardLink
+                                href="/templates"
+                                className="underline hover:text-neutral-100"
+                            >
+                                Note Templates
+                            </StandardLink>
+                        </li>
+                    </ul>
+
+                    <div className="mt-10">
+                        <h2 className="text-neutral-100 text-4xl font-bold">
+                            Recent Notes:
+                        </h2>
                     </div>
+                    <ul className="list-disc list-inside mt-4 ml-4">
+                        {recentNotesList()}
+                    </ul>
                 </div>
             </div>
         </AuthenticatedLayout>
