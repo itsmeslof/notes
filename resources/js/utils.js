678480236b0b92e81e4ba1677d725b20a0691379
Slof <slof@hey.com>
2023-06-27 17:55:49 -0500

    Convert date to UTC in dayjs

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -1,9 +1,11 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
 
+dayjs.extend(utc);
 dayjs.extend(relativeTime);
 
-export const humanizeDate = (date) => dayjs(date).fromNow();
+export const humanizeDate = (date) => dayjs.utc(date).fromNow();
 
 export const defaultNoteContent = `---
 title: Example Note
