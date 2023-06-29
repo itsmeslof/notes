efbf8c5d05f23dcea3bb2228f6b7cd7ba7edd566
Slof <slof@hey.com>
2023-06-28 19:24:55 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import laravel from "laravel-vite-plugin";
-import { refreshPaths } from "laravel-vite-plugin";
-
-console.log(refreshPaths);
 
 export default defineConfig({
     plugins: [
